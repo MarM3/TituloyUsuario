2a98825a7d88bd955079ffa733e7d6e18a921fca
Mar Mazuelas <mm.zlsmng@gmail.com>
2019-05-03 11:04:34 +0200

    Soluciona el Js

--- js/codigo.js
+++ js/codigo.js
@@ -1,6 +1,8 @@
 function datos() {
     let titulo = prompt("Introduce el título de la página: ");
 
+    let pepe = {};
+
     let patron = /[,;·$%&\/()=?¿<>:!¡+]/i;
     if (!patron.test(titulo) && titulo !== null && titulo !== undefined) {
         document.getElementById("Titulo").innerHTML = titulo;
@@ -8,33 +10,26 @@ function datos() {
         titulo = prompt("Introduzca un nuevo título: ");
     }
 
-    patron = /0-9.,;·$%&\/()=?¿<>:!¡+/i;
-    let nombre = prompt("Introduce el nombre del Usuario: ");
-    if (!patron.test(nombre) && nombre === null && nombre === undefined) {
-        nombre = prompt("Introduzca un nuevo nombre: ");
+    patron = /[0-9.,;·$%&\/()=?¿<>:!¡+]/i;
+    pepe.nombre = prompt("Introduce el nombre del Usuario: ");
+    if (pepe.nombre === null && pepe.nombre === undefined) {
+        pepe.nombre = prompt("Introduzca un nuevo nombre: ");
     }
-    let apellido1 = prompt("Introduce el primer apellido del Usuario: ");
-    if (!patron.test(apellido1) && apellido1 === null && apellido1 === undefined) {
-        apellido1 = prompt("Introduzca, de nuevo, el apellido: ");
+    pepe.apellido1 = prompt("Introduce el primer apellido del Usuario: ");
+    if (!patron.test(pepe.apellido1) && pepe.apellido1 === null && pepe.apellido1 === undefined) {
+        pepe.apellido1 = prompt("Introduzca, de nuevo, el apellido: ");
     }
-    let fechNaci = prompt("Introduzca la fecha de nacimiento del Usuario: ");
+    pepe.fechNaci = prompt("Introduzca la fecha de nacimiento del Usuario: ");
     //let patron2 = /a-zA-Z.,;·$%&\()=?¿<>:!¡+/; !patron2.test(fechNaci) &&
-    if (fechNaci === null && fechNaci === undefined) {
-        fechNaci = prompt("Introduzca, de nuevo, la fecha de nacimiento: ");
+    if (pepe.fechNaci === null && pepe.fechNaci === undefined) {
+        pepe.fechNaci = prompt("Introduzca, de nuevo, la fecha de nacimiento: ");
     }
-    let lugarResu = prompt("Introduzca el lugar de residencia del Usuario: ");
-    if (!patron.test(lugarResu) && lugarResu === null && lugarResu === undefined) {
-        nombre = prompt("Introduzca, de nuevo, el lugar de residencia: ");
-    }
-}
-    function Usuario(parNombre, parApe1, parFechaN, parLugarR) {
-        this.nombre = parNombre;
-        this.apellido1 = parApe1;
-        this.fechNaci = parFechaN;
-        this.lugarResu = parLugarR;
+    pepe.lugarResu = prompt("Introduzca el lugar de residencia del Usuario: ");
+    if (pepe.lugarResu === null && pepe.lugarResu === undefined) {
+        pepe.lugarResu = prompt("Introduzca, de nuevo, el lugar de residencia: ");
     }
 
-    let pepe = new Usuario(nombre, apellido1, fechNaci, lugarResu);
     for (let prop in pepe) {
-        document.getElementById("mostrarDatos").innerHTML = prop + "\n";
+        document.getElementById("mostrarDatos").innerHTML += pepe[prop] + "\n";
     }
+}
