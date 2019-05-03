35b8632636fdca6e90da4f74586dde4fd0099b0b
Mar Mazuelas <mm.zlsmng@gmail.com>
2019-05-03 11:38:25 +0200

    Arreglos en Js

--- js/codigo.js
+++ js/codigo.js
@@ -1,33 +1,30 @@
 function datos() {
-    let titulo = prompt("Introduce el título de la página: ");
-
+    let titulo;
     let pepe = {};
 
     let patron = /[,;·$%&\/()=?¿<>:!¡+]/i;
-    if (!patron.test(titulo) && titulo !== null && titulo !== undefined) {
-        document.getElementById("Titulo").innerHTML = titulo;
-    } else {
+    do {
         titulo = prompt("Introduzca un nuevo título: ");
-    }
+    } while (patron.test(titulo) || titulo === null || titulo === undefined);
+    document.getElementById("Titulo").innerHTML = titulo;
 
     patron = /[0-9.,;·$%&\/()=?¿<>:!¡+]/i;
-    pepe.nombre = prompt("Introduce el nombre del Usuario: ");
-    if (pepe.nombre === null && pepe.nombre === undefined) {
-        pepe.nombre = prompt("Introduzca un nuevo nombre: ");
-    }
-    pepe.apellido1 = prompt("Introduce el primer apellido del Usuario: ");
-    if (!patron.test(pepe.apellido1) && pepe.apellido1 === null && pepe.apellido1 === undefined) {
-        pepe.apellido1 = prompt("Introduzca, de nuevo, el apellido: ");
-    }
-    pepe.fechNaci = prompt("Introduzca la fecha de nacimiento del Usuario: ");
+    do {
+        pepe.nombre = prompt("Introduzca el nombre del Usuario: ");
+    }while (patron.test(pepe.nombre) || pepe.nombre === null || pepe.nombre === undefined);
+
+    do {
+        pepe.apellido1 = prompt("Introduce el primer apellido del Usuario: ");
+    }while  (patron.test(pepe.apellido1) || pepe.apellido1 === null || pepe.apellido1 === undefined);
+
     //let patron2 = /a-zA-Z.,;·$%&\()=?¿<>:!¡+/; !patron2.test(fechNaci) &&
-    if (pepe.fechNaci === null && pepe.fechNaci === undefined) {
-        pepe.fechNaci = prompt("Introduzca, de nuevo, la fecha de nacimiento: ");
-    }
-    pepe.lugarResu = prompt("Introduzca el lugar de residencia del Usuario: ");
-    if (pepe.lugarResu === null && pepe.lugarResu === undefined) {
-        pepe.lugarResu = prompt("Introduzca, de nuevo, el lugar de residencia: ");
-    }
+    do {
+        pepe.fechNaci = prompt("Introduzca la fecha de nacimiento del Usuario: ");
+    }while (pepe.fechNaci === null && pepe.fechNaci === undefined);
+
+    do {
+        pepe.lugarResu = prompt("Introduzca el lugar de residencia del Usuario: ");
+    }while (patron.test(pepe.lugarResu) || pepe.lugarResu === null || pepe.lugarResu === undefined);
 
     for (let prop in pepe) {
         document.getElementById("mostrarDatos").innerHTML += pepe[prop] + "\n";
